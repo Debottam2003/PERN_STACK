86ef2b91028c5331dfc40bda213b003498d59c80
Debottam Kar <debottamkar2003@gmail.com>
2025-06-09 01:16:37 +0530

    recap and formatting

--- Backend/ai.js
+++ Backend/ai.js
@@ -8,11 +8,11 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 const prompt = "welcome";
 
 async function generateContentCustom(prompt) {
-    try{
-    const result = await model.generateContent(prompt);
-    //console.log(result.response.text());
-    return result.response.text();
-    } catch (error) {   
+    try {
+        const result = await model.generateContent(prompt);
+        //console.log(result.response.text());
+        return result.response.text();
+    } catch (error) {
         console.log(error);
     }
 }
@@ -29,16 +29,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render("ai");
-}); 
+});
 
 app.post('/ai', async (req, res) => {
     let ai_data = await generateContentCustom(req.body.prompt);
-    let clean_data = ai_data.split('\n').filter((line) => {return line.length > 0});
-    res.render("ai", {ai_data : clean_data});
+    let clean_data = ai_data.split('\n').filter((line) => { return line.length > 0 });
+    res.render("ai", { ai_data: clean_data });
 });
 
 app.listen(process.env.PORT, () => {
